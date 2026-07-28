059f66df145d356ea1c1770440ec6f3c0e3ff9d7
Hiero <hiero.dhp@gmail.com>
2026-07-28 09:31:46 +0700

    fix(tasks): change the dynamic route GET parameter ctx data type

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -1,10 +1,10 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(_: NextRequest,ctx: {params: Promise<{id: number}>}) {
+export async function GET(_: NextRequest, ctx: {params: Promise<{id: string}>}) {
   const { id } = await ctx.params;
   const task = await prisma.task.findUnique({
-    where: { id: id },
+    where: { id: Number(id) },
   });
 
   // success response
