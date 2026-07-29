dc93213c8b459f85e14662eb9e407590b2fcf6ef
Hiero <hiero.dhp@gmail.com>
2026-07-29 09:02:34 +0700

    feat(auth): add authorization for each of the tasks endpoint

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -1,13 +1,30 @@
+import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-  _: NextRequest,
+  request: NextRequest,
   ctx: { params: Promise<{ id: string }> },
 ) {
+  const session = await auth.api.getSession({
+    headers: request.headers,
+  });
+  const userId = session?.user?.id;
+
+  // error response
+  if (!userId) {
+    return NextResponse.json(
+      { status: "error", message: "Unauthorized" },
+      { status: 401 },
+    );
+  }
+
   const { id } = await ctx.params;
   const task = await prisma.task.findUnique({
-    where: { id: Number(id) },
+    where: {
+      id: Number(id),
+      userId: userId,
+    },
   });
 
   // success response
@@ -25,12 +42,32 @@ export async function PATCH(
   request: NextRequest,
   ctx: { params: Promise<{ id: string }> },
 ) {
+  const session = await auth.api.getSession({
+    headers: request.headers,
+  });
+  const userId = session?.user?.id;
+
+  // error response
+  if (!userId) {
+    return NextResponse.json(
+      { status: "error", message: "Unauthorized" },
+      { status: 401 },
+    );
+  }
+
   const { id } = await ctx.params;
   const body = await request.json();
+  const { title, description } = body;
 
   const task = await prisma.task.update({
-    where: { id: Number(id) },
-    data: body,
+    where: {
+      id: Number(id),
+      userId: userId,
+    },
+    data: {
+      title: title,
+      description: description,
+    },
   });
 
   // success response
@@ -45,13 +82,29 @@ export async function PATCH(
 }
 
 export async function DELETE(
-  _: NextRequest,
+  request: NextRequest,
   ctx: { params: Promise<{ id: string }> },
 ) {
+  const session = await auth.api.getSession({
+    headers: request.headers,
+  });
+  const userId = session?.user?.id;
+
+  // error response
+  if (!userId) {
+    return NextResponse.json(
+      { status: "error", message: "Unauthorized" },
+      { status: 401 },
+    );
+  }
+
   const { id } = await ctx.params;
 
   const deletedTask = await prisma.task.delete({
-    where: { id: Number(id) },
+    where: {
+      id: Number(id),
+      userId: userId,
+    },
   });
 
   // success response
--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,3 +1,4 @@
+import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -5,10 +6,24 @@ export async function POST(request: NextRequest) {
   const body = await request.json();
   const { title, description } = body;
 
+  const session = await auth.api.getSession({
+    headers: request.headers,
+  });
+  const userId = session?.user.id;
+
+  // error response
+  if (!userId) {
+    return NextResponse.json(
+      { status: "error", message: "Unauthorized" },
+      { status: 401 },
+    );
+  }
+
   const task = await prisma.task.create({
     data: {
       title: title,
       description: description,
+      userId: userId,
     },
   });
 
@@ -23,8 +38,25 @@ export async function POST(request: NextRequest) {
   );
 }
 
-export async function GET() {
-  const tasks = await prisma.task.findMany();
+export async function GET(request: NextRequest) {
+  const session = await auth.api.getSession({
+    headers: request.headers,
+  });
+  const userId = session?.user.id;
+
+  // error response
+  if (!userId) {
+    return NextResponse.json(
+      { status: "error", message: "Unauthorized" },
+      { status: 401 },
+    );
+  }
+
+  const tasks = await prisma.task.findMany({
+    where: {
+      userId: userId,
+    },
+  });
 
   // success response
   return NextResponse.json(
